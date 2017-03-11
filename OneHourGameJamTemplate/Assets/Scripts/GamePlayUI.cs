655cc1d33181c8ee18d6ec02b272883522d5b870
maxiaoxiao1991 <maxiaoxiao1991@gmail.com>
2017-03-11 21:55:25 +0200

    UI template

---
+++ OneHourGameJamTemplate/Assets/Scripts/GamePlayUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePlayUI : MonoBehaviour {
+	public static GamePlayUI gamePlayUI;
+
+	void Awake(){
+		gamePlayUI = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void Restart(){
+		SceneManager.LoadScene ("GameScene");
+	}
+
+	public void Quit(){
+		SceneManager.LoadScene ("MenuScene");
+	}
+}
